511b2e4655f6489ebc263d39879bac9064566001
micahmeadows <micahstlp@gmail.com>
2022-04-05 19:21:24 -0400

    Make shell script to run sorting tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Running tests for name sort app..."
-dart test
+echo "Running tests for normal sorting...";
+dart test test/string_sorting/normal_sorting_test.dart -r expanded
+echo
+
+echo "Running tests for reverse sorting...";
+dart test test/string_sorting/reverse_sorting_test.dart -r expanded
+echo
+
