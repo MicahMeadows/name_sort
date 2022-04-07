642306e3bb4c67deb3c6ab1a182335df36791e98
micahmeadows <micahstlp@gmail.com>
2022-04-06 23:19:04 -0400

    Add file tests and update test shell script

--- run_tests.sh
+++ run_tests.sh
@@ -8,3 +8,7 @@ echo "Running tests for reverse sorting...";
 dart test test/string_sorting/reverse_sorting_test.dart -r expanded
 echo
 
+echo "Running tests for file input repository...";
+dart test test/file_input/file_name_repository_test.dart -r expanded
+echo
+
